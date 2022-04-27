fae6735d8b4d087e27f7e0d256a5391f02f1cc08
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-27 13:31:00 -0300

    Todo app - Usando os reducers para marcar como feito e desfeito

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -16,9 +16,24 @@ export const search = () => {
 }
 
 export const add = (description) => {
-    const request = axios.post(URL, { description })
-    return [
-        { type: 'TODO_ADDED', payload: request },
-        search()
-    ]
+    return dispatch => {
+        axios.post(URL, {description})
+            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data}))
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsDone = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true} )
+            .then(resp => dispatch({type: 'TODO_MARKED_AS_DONE', payload: resp.data}))
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsPending = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false})
+            .then(resp => dispatch(search()))
+    }
 }
